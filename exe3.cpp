8ca8294cf4a10676f67b12503778f296c3d0d6aa
gudeh <gudehh@gmail.com>
2020-11-16 00:28:05 -0200

    recursao retornando,mas resultado nao otimo

--- exe3.cpp
+++ exe3.cpp
@@ -14,6 +14,7 @@
 #include <cstdlib>
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -26,8 +27,50 @@ struct item{
     int lucro;
 };
 
-int opt(item to_load, vector<item> itens_restantes){
+int opt(vector<item> itens_restantes, vector<float> capacidade_atual){
+    cout<<"OPT CALL!"<<endl;
+    //caso base
     
+    int total_cap=0; for(auto x:capacidade_atual) total_cap+=x;
+    if(itens_restantes.size()==0 || total_cap<=0)
+    {
+        cout<<"Returning 0."<<endl;
+        return 0;
+    }
+    else
+    {
+        cout<<"else"<<endl;
+        item current=itens_restantes[0];
+        //checando se o item a ser inserido cabe em algum caminhao.
+        bool fit=false; int index=0,ret=0;
+        for(int i=0;i<capacidade_atual.size();i++)
+        {
+            if(current.peso<capacidade_atual[i])
+            {
+                index=i;
+                fit=true;
+                break;
+            }
+        }
+        //caso o item nao caiba
+        if(!fit)
+        {
+            itens_restantes.erase(itens_restantes.begin());
+            ret=opt(itens_restantes,capacidade_atual);
+            //TODO: guardar lucro perdido
+        }
+        //caso o item caiba no caminhao em index
+        else
+        {
+            //TODO: peso negativo?
+            vector<float> cap_aux=capacidade_atual;
+            cap_aux[index]=cap_aux[index]-current.peso;
+            itens_restantes.erase(itens_restantes.begin());
+            ret=max(current.lucro+opt(itens_restantes,cap_aux),
+                    opt(itens_restantes,capacidade_atual));
+        }
+        return ret;
+    }
 }
 
 //Retorno: os itens que não foram entregues (vector) e o lucro perdido (int).
@@ -38,27 +81,26 @@ pair<vector<int>,int> exercicioDois(vector<float> capacidade_caminhoes, vector<u
     cout<<"Numero de itens:"<<peso_itens.size()<<endl<<"\tPeso total:"; aux=0;  for(auto x : peso_itens)        aux+=x; cout<<aux<<endl;
     cout<<"\tLucro de todos itens:";   aux=0;  for(auto x : lucro_itens)        aux+=x; cout<<aux<<endl;
     
-    vector<item> all_itens;
+    vector<item> all_items;
     for(int x=0;x<peso_itens.size();x++)
     {
         item item_obj;
         item_obj.id=x;
         item_obj.peso=peso_itens[x];
         item_obj.lucro=lucro_itens[x];
-        all_itens.push_back(item_obj);
+        all_items.push_back(item_obj);
     }
-        
-    
-    
-    
-    
-    
-//    opt();
+    int ret=0;
+    ret=opt(all_items,capacidade_caminhoes);
+    cout<<"RETORNO:"<<ret<<endl;
 }
+
+
+
 int main(int argc, char** argv) {
     
-    vector<float> capacidades={11,14};
-    vector<unsigned int> pesos={3,3,4,4,5,7},lucros={1,2,3,4,5,10};
+    vector<float> capacidades={10,15};
+    vector<unsigned int> pesos={10,10,10,10,10},lucros={1,2,3,4,5};
     
     if(pesos.size() == lucros.size())
         exercicioDois(capacidades,pesos,lucros);
